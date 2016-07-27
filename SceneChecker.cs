d265d17c8fd77a9613502d7c853e4969ad43e30e
mnsuka <a.casual.meeting@gmail.com>
2016-07-27 19:49:53 +0900

    add scene change check code

---
+++ SceneChecker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneChecker : SingletonBehaviour< SceneChecker > {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//	scene 名を表示する
+		var	name	= SceneManager.GetActiveScene().name ;
+
+		if (Input.GetKeyDown (KeyCode.N)) {
+			if ("test_code" == name) {
+				SceneManager.LoadScene ("test_scene");
+			}
+			else {
+				SceneManager.LoadScene ("test_code");
+			}
+		}
+	}
+
+	void OnGUI(){
+		//	scene 名を表示する
+		GUI.Label( new Rect( 20, 40, 80, 20 ), SceneManager.GetActiveScene().name );
+	}
+}
